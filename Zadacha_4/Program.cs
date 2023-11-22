6c6b36859654830b39e37eabf6be99c53fb4ac02
Dima <fsgz@mail.ru>
2023-11-23 01:09:49 +0200

    решение перечисление всех чисел через запятую, вводимого натурального числа

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// РЕШЕНИЕ 1 Не выводит запятую на последнем числе
+// Console.Write("Введите любое натуральное число: ");
+// string inputNum = (Console.ReadLine()!);
+// int number = int.Parse(inputNum);
+// string outNumLine = string.Empty;
+// int count = 0;
+// while (count < number)
+// {
+//     outNumLine = outNumLine + count + ",";
+//     count = count + 1;
+// }
+// Console.WriteLine(outNumLine + number);
+
+// РЕШЕНИЕ 2 СДЕЛАЛ С ПРИМЕРА ИЗ СЕМИНАРА 1, ТАМ СЧИАЛИ ОТ -n ДО n.
+// Console.Write("Введите любое натуральное число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// for (int i = 0; i <= number; i++)
+//     Console.Write($"{i},");
+
+// РЕШЕНИЕ 3 СДЕЛАЛ С ПРИМЕРА ИЗ СЕМИНАРА 1, ТАМ СЧИАЛИ ОТ -n ДО n.
+// Console.Write("Введите любое натуральное число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// int i = 0;
+// while (i <= number)
+// {
+//     Console.Write($"{i},");
+//     i++;
+// }
+// Так и не понял как сделать что бы счтило т отрицательные числа(
